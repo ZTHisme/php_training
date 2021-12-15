2f1efafed58f3138580bb69717ba0350bfca4585
Zin-Thu-Htet <zth.chung@gmail.com>
2021-12-15 19:38:26 +0630

    [master] Fix Reviews in Tutorial 1 and 2

--- tutorial/tutorial_01/index.php
+++ tutorial/tutorial_01/index.php
@@ -6,29 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tutorial01</title>
-    <style>
-        table {
-            width: 270px;
-            border: 2px solid #000;
-        }
-
-        #td1 {
-            height: 30px;
-            width: 30px;
-            background-color: #FFFFFF;
-        }
-
-        #td2 {
-            height: 30px;
-            width: 30px;
-            background-color: #000000;
-        }
-    </style>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <table>
         <?php
+        /**
+         *Create chess board 
+         */
         for ($row = 1; $row <= 8; $row++) {
             echo "<tr>";
             for ($col = 1; $col <= 8; $col++) {
