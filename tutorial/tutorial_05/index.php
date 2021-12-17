d03e6bfbe8df3ce059be0f92749461da8f184e6c
Zin-Thu-Htet <zth.chung@gmail.com>
2021-12-17 16:51:52 +0630

    [master] finished php tutorial 5

---
+++ tutorial/tutorial_05/index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tutorial05</title>
+</head>
+
+<body>
+  <?php
+    /**
+     * The data show from text file on browser
+     */
+    echo '1) Content of text file<br>';
+    $file = fopen("sample.txt", "r");
+
+    while (!feof($file)) {
+        echo fgets($file) . "<br />";
+    }
+    fclose($file);
+    ?>
+  <?php
+    /**
+     * The data show from csv file on browser
+     */
+    echo '<br>2) Content of CSV file<br>';
+    echo '<table border="1px" cellspacing="0" cellpadding="0">';
+    $csv_file = fopen("sample.csv", "r");
+    while ($read_data = fgetcsv($csv_file, 1024, ',')) {
+        $column_count = count($read_data);
+        echo '<tr>';
+        for ($col = 0; $col < $column_count; $col++) {
+            echo "<td>" . $read_data[$col] . "</td>";
+        }
+        echo '</tr>';
+    }
+    fclose($csv_file);
+    echo '</table>';
+    ?>
+  <?php
+    /**
+     * The data show from xlsx file on browser
+     */
+    echo '<br>3) Content of excel file<br>';
+    require_once "./library/vendor/shuchkin/simplexlsx/src/SimpleXLSX.php";
+    if ($xlsx = SimpleXLSX::parse('sample.xlsx')) {
+        echo '<table><tbody>';
+        $i = 0;
+
+        foreach ($xlsx->rows() as $elt) {
+            if ($i == 0) {
+                echo "<tr><th>" . $elt[0] . "</th><th>" . $elt[1] . "</th></tr>";
+            } else {
+                echo "<tr><td>" . $elt[0] . "</td><td>" . $elt[1] . "</td></tr>";
+            }
+
+            $i++;
+        }
+
+        echo "</tbody></table>";
+    } else {
+        echo SimpleXLSX::parseError();
+    }
+    ?>
+  <?php
+    /**
+     * The data show from word file on browser
+     */
+    echo '<br>4) Content of doc file<br>';
+    $filename = "sample.doc";
+    function readWord($filename)
+    {
+        if (file_exists($filename)) {
+            if (($fh = fopen($filename, 'r')) !== false) {
+                $headers = fread($fh, 0xA00);
+
+                $char = (ord($headers[0x21C]) - 1);
+
+                $textLength = ($char);
+
+                $extracted_plaintext = fread($fh, $textLength);
+
+                return nl2br($extracted_plaintext);
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+    echo readWord($filename);
+    ?>
+
+</body>
+
+</html>
